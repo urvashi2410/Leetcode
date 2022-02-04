4a5b73b864071e8c608fcf08220ec78244f3f1d7
urvashi2410 <urvashianand0906@gmail.com>
2022-02-04 18:34:00 +0530

    basic linked list

---
+++ Easy/203.remove-linked-list-elements.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=203 lang=cpp
+ *
+ * [203] Remove Linked List Elements
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == NULL){
+            return head;
+        }
+
+        ListNode* curr = head;
+        while(curr != NULL && curr->next != NULL){
+            if(curr->next->val == val){
+                curr->next = curr->next->next;
+            }
+            else{
+                curr = curr->next;
+            }
+        }
+
+        if(head->val == val){
+            head = head->next;
+        }
+        return head;
+    }
+};
+// @lc code=end
+
