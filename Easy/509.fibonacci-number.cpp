7d7078231267dc7dda0ca02d991324fe28c77e10
urvashi2410 <urvashianand0906@gmail.com>
2022-02-12 14:28:53 +0530

    fibonacci

--- Easy/509.fibonacci-number.cpp
+++ Easy/509.fibonacci-number.cpp
@@ -7,6 +7,19 @@
 // @lc code=start
 class Solution {
 public:
+    // int fib(int n) {
+    //     if(n < 2){
+    //         return n;
+    //     }
+    //     vector<int>dp(n+1, -1);
+    //     dp[0] = 0;
+    //     dp[1] = 1;
+    //     for(int i = 2; i <= n; i++){
+    //         dp[i] = dp[i-1] + dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
+    
     void multiply(int M[2][2], int A[2][2]){
         int fvalue = M[0][0] * A[0][0] + M[0][1] * A[1][0];
         int svalue = M[0][0] * A[0][1] + M[0][1] * A[1][1];
